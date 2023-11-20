dd3091ee56c886e48368830e72b69207078c97fd
Fady Yassa <fyassa@sumerge.com>
2023-11-20 21:32:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'style-calculator';
+  designWidth = new FormControl('', [Validators.required]);
+  designHeight = new FormControl('', [Validators.required]);
+  actual = new FormControl('', [Validators.required]);
+  font: string = '0';
+  horizontal: string = '0';
+  vertical: string = '0'
+
+  copyMessage(val: string) {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
+  calculate() {
+    if(this.designWidth.valid&&this.designHeight.valid&&this.actual.valid){
+      this.font=((parseFloat(this.actual.value!)/parseFloat(this.designHeight.value!))*100).toFixed(2)
+      this.vertical=((parseFloat(this.actual.value!)/parseFloat(this.designHeight.value!))*100).toFixed(2)
+      this.horizontal=((parseFloat(this.actual.value!)/parseFloat(this.designWidth.value!))*100).toFixed(2)
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
